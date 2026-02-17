c796c6a921dd6924c8e0e9559bc279938af5fe11
leifjones06 <leifjones06@gmail.com>
2026-02-17 09:03:09 -0600

    modified:   FirstManager.hpp 	modified:   SecondManager.hpp 	modified:   ThirdManager.hpp

---
+++ FirstManager.hpp
@@ -0,0 +1,14 @@
+#ifndef FIRSTMANAGER_H
+#define FIRSTMANAGER_H
+#include <string>
+using namespace std;
+class FirstManager
+{
+    private:
+    string name;
+    bool gender;
+    int age;
+    int numFam;
+    int DOB;
+};
+#endif
