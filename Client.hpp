777e0201605f7d36b7b0455f8b3807c51304df0b
leiftheleaf <127000228+leiftheleaf@users.noreply.github.com>
2026-02-21 12:21:50 -0600

    Modified Client.hpp

---
+++ Client.hpp
@@ -0,0 +1,13 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+#include <string>
+class Client
+{
+    public:
+    std::string name;
+    char gender;
+    int age;
+    int familyMembers;
+    std::string dateOfBirth;
+};
+#endif
