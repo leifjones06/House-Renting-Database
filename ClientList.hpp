d77f0bbf79571e7feb0589885e8e4ec26f1c5e4e
leiftheleaf <127000228+leiftheleaf@users.noreply.github.com>
2026-02-21 12:36:37 -0600

    Modified ClientList.hpp

---
+++ ClientList.hpp
@@ -0,0 +1,28 @@
+#ifndef CLIENTLIST_HPP
+#define CLIENTLIST_HPP
+#include "Client.hpp"
+class ClientList
+{
+    private:
+    struct Node
+    {
+        Client data;
+        Node* next;
+        Node* prev;
+        Node(const Client& c) : data(c), next(nullptr), prev(nullptr) {}
+    };
+    Node* head;
+    Node* tail;
+    public:
+    ClientList();
+    ~ClientList();
+    void insertAtBeginning(const Client& c);
+    void insertAtEnd(const Client& c);
+    void insertAfter(const Client& target, const Client& c); //Search by name/age/gender
+    Client* getFirst();
+    Client* getLast();
+    Client getClient(const std::string& name);
+    Client* removeMostRecent();
+    Client* removeOldest();
+};
+#endif
